8bf56ef965f437c3283d34678434edd9b9496e02
topazb <topazb@gmail.com>
2018-10-18 12:31:35 +0300

    just added .

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,10 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-/**
-* This is astring formater
-*
-*/
+
+  /**
+  * This is a string formater.
+  */
+
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
