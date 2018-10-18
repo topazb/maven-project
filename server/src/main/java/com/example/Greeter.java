8a639ee732e3401e8b373809997963987b29d18c
topazb <topazb@gmail.com>
2018-10-18 12:44:17 +0300

    fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -18,8 +18,7 @@ public class Greeter {
   * @param someone
   */
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
